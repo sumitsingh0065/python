a760e6d1f8b3a6896df02676f33df8337be52634
Sumit Singh <152257946+sumitsingh0065@users.noreply.github.com>
2025-11-18 00:15:14 +0530

    Create py2.py

---
+++ py2.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+print("Your random password is:", generate_password())
